4f391f29908e464512fdaba4b5fa44c129195aea
Murad Narimanli <murad.narimanli@timesoft.local>
2021-09-30 15:37:24 +0400

    category finished

--- src/components/Layout/Routing/Routing.js
+++ src/components/Layout/Routing/Routing.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Redirect, Route, Switch} from "react-router-dom";
-import Home from "../../Pages/Admin/Home/Home";
-
+import Home from "../../Pages/Home/Home";
+import Categories from "../../Pages/Categories/Categories"
 function Routing(props) {
     return (
         <Switch>
             <Route exact path={`/`} component={Home} />
+            <Route exact path={`/categories`} component={Categories} />
             <Redirect to="/" />
             <Route path="/">
                 <p className="flex all-center h-100vh">Not found</p>
