e9522f6bad0f7e09e33f5da86423488705d1d1c8
Murad Narimanli <murad.narimanli@timesoft.local>
2021-09-30 15:59:41 +0400

    persons & tables created

--- src/components/Layout/Routing/Routing.js
+++ src/components/Layout/Routing/Routing.js
@@ -2,11 +2,15 @@ import React from 'react';
 import {Redirect, Route, Switch} from "react-router-dom";
 import Home from "../../Pages/Home/Home";
 import Categories from "../../Pages/Categories/Categories"
+import Tables from "../../Pages/Tables/Tables";
+import Persons from "../../Pages/Persons/Persons";
 function Routing(props) {
     return (
         <Switch>
             <Route exact path={`/`} component={Home} />
             <Route exact path={`/categories`} component={Categories} />
+            <Route exact path={`/tables`} component={Tables} />
+            <Route exact path={`/persons`} component={Persons} />
             <Redirect to="/" />
             <Route path="/">
                 <p className="flex all-center h-100vh">Not found</p>
