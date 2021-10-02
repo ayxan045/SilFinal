db0205d4aedc911a9f79a8cd52c550515d6df137
Murad Narimanli <murad.narimanli@timesoft.local>
2021-10-03 00:09:14 +0400

    little ui fixes

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Row, Col, Button} from 'antd'
 import {PicCenterOutlined} from "@ant-design/icons";
 import {Link} from "react-router-dom";
@@ -9,15 +9,20 @@ import OrderStatistics from "../../Elements/OrderStatistics";
 import OrderItemStatistics from "../../Elements/OrderItemStatistics";
 import MenuSlider from "../../Elements/MenuSlider";
 function Home(props) {
-
-
+    const [show,setShow] = useState(true)
+    useEffect(()=>{
+        window.addEventListener("resize", () => {
+            setShow(window.innerWidth > 1200)
+        });
+        setShow(window.innerWidth > 1200)
+    })
     return (
         <Row gutter={[10, 10]}>
             <Col xs={24}>
-                <div className="border flex-between animated fadeInDown page-heading flex p-2 mt-0 bg-white">
+                <div className="border flex-between flex-wrap animated fadeInDown page-heading flex p-2 mt-0 bg-white">
                     <div className="page-name">
                         <PicCenterOutlined className="f-20 mr5-15" />
-                        <span className="f-20 bold">Şirkət haqqında və statistika</span>
+                        <span className="f-20 bold">Informasiya</span>
                     </div>
                     <div className="flex">
                         <Link
@@ -26,20 +31,6 @@ function Home(props) {
                             }}
                         >
                             <Button className={'mr-15 animated zoomIn'} type={"primary"}>Sifarişlər</Button>
-                        </Link>
-                        <Link
-                            to={{
-                                pathname: `/menu`,
-                            }}
-                        >
-                            <Button className={'mr-15 animated zoomIn'} type={"primary"}>Menyu</Button>
-                        </Link>
-                        <Link
-                            to={{
-                                pathname: `/tables`,
-                            }}
-                        >
-                            <Button className={'animated zoomIn'} type={"primary"}>Masalar</Button>
                         </Link>
                     </div>
                 </div>
@@ -58,10 +49,12 @@ function Home(props) {
                             <OrderItemStatistics/>
                         </Col>
 
+
+                        {show &&
                         <Col xs={24}>
                             <MenuSlider/>
                         </Col>
-
+                        }
                     </Row>
                 </div>
             </Col>
